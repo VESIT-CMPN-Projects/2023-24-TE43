2224bac6fbcfee98e6753eddfbba599089090dc6
Dhruv Aswani <2021.dhruv.aswani@ves.ac.in>
2023-09-23 22:02:44 +0530

    Security UI in progress

---
+++ app/src/main/java/com/example/empoweher/Security.kt
@@ -0,0 +1,17 @@
+package com.example.empoweher
+
+import androidx.compose.foundation.layout.Row
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun safety(){
+    Text(text = "Safety")
+
+    Row(
+
+    ){
+
+    }
+
+}
