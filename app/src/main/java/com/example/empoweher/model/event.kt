a6baa31fa879f57eb06dfaa92b70dc211b280e64
Amansande <amansande1934@gmail.com>
2023-11-11 18:16:07 +0530

    Insert done fetch pending

---
+++ app/src/main/java/com/example/empoweher/model/event.kt
@@ -0,0 +1,6 @@
+package com.example.empoweher.model
+
+data class event (
+    var name : String?=null,var place:String?=null
+)
+
--- app/src/main/java/com/example/empoweher/screen/temp2.kt
+++ app/src/main/java/com/example/empoweher/screen/temp2.kt
@@ -1,9 +1,37 @@
 package com.example.empoweher.screen
 
+import android.util.Log
+import android.widget.Toast
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.material3.Card
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.empoweher.MainActivity
+import com.example.empoweher.model.event
+import com.example.empoweher.viewmodel.mainviewmodel
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
 
 @Composable
+fun temp2() {
+var vmodel:mainviewmodel by viewModels()
 
-fun temp2(){
+
 
 }
