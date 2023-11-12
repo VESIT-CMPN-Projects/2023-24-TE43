e06bd49981d9f54f781e506c1d8f7c4763613124
PCAPS <praful0124@gmail.com>
2023-11-12 10:04:08 +0530

    EMERGENCY-LIST-UPDATE

---
+++ app/src/main/java/com/example/empoweher/screen/EmergencyListUpdate.kt
@@ -0,0 +1,10 @@
+package com.example.empoweher.screen
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun EmergencyListUpdate(){
+    Text(text = "JWSH")
+
+}
