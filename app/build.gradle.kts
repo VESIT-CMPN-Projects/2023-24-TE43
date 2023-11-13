c1e96e47f0de7ed3ca477522833912413fb719ca
PCAPS <praful0124@gmail.com>
2023-11-13 19:03:44 +0530

    Started Room Experimental

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -83,11 +83,18 @@ dependencies {
     implementation ("io.coil-kt:coil-compose:2.2.2")
 
     //Room
-
-
+    implementation ("androidx.room:room-runtime:2.6.0")
+    kapt ("androidx.room:room-compiler:2.6.0")
+    
     implementation("androidx.room:room-ktx:2.6.0")
+
     //Dagger - Hilt
     implementation ("com.google.dagger:hilt-android:2.46.1")
     kapt ("com.google.dagger:hilt-android-compiler:2.46.1")
     kapt ("androidx.hilt:hilt-compiler:1.0.0")
+
+    //Coroutines
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
+
+
 }
