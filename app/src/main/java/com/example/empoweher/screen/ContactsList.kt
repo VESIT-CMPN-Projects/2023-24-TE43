a0c0aa974df2fe3627eccd0de57dd0dc3fc8cf7b
DhruvAswani <2021.dhruv.aswani@ves.ac.in>
2023-11-14 16:19:27 +0530

    Sliding Delete Done

--- app/src/main/java/com/example/empoweher/screen/ContactsList.kt
+++ app/src/main/java/com/example/empoweher/screen/ContactsList.kt
@@ -2,8 +2,10 @@ package com.example.empoweher.screen
 
 import android.annotation.SuppressLint
 import android.util.Log
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -13,13 +15,18 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Delete
 import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.DismissDirection
 import androidx.compose.material3.DismissValue
+import androidx.compose.material3.Divider
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
 import androidx.compose.material3.SwipeToDismiss
 import androidx.compose.material3.Text
 import androidx.compose.material3.rememberDismissState
@@ -32,6 +39,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.room.Room
@@ -45,6 +53,7 @@ import kotlinx.coroutines.async
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.toList
 import kotlinx.coroutines.launch
+import com.example.empoweher.R
 
 @SuppressLint("CoroutineCreationDuringComposition")
 @Composable
@@ -63,17 +72,7 @@ fun ContactsList(){
 
 //    Text(text = List.toString())
 
-    LazyColumn(
-        modifier=Modifier.fillMaxWidth(),
-        content = {
-            items(List){item->
-                Contacts(fName = item.firstName, lName =item.lastName , pNum =item.phoneNumber , checked =item.emergency )
-
-
-            }
-        }
-    )
-
+    lazy(list = List.toMutableList())
 
 
 
@@ -86,13 +85,11 @@ suspend fun func(database: ContactDatabase){
 @Composable
 fun Contacts(fName: String, lName: String, pNum: String,checked: Boolean){
     Card(
-        elevation = CardDefaults.cardElevation(defaultElevation = 8.dp) ,
+        elevation = CardDefaults.cardElevation(defaultElevation = 20.dp) ,
         modifier = Modifier
             .padding(8.dp)
             .fillMaxWidth()
-            .clickable {
 
-            }
     ){
         Column(
             horizontalAlignment = Alignment.CenterHorizontally,
@@ -118,15 +115,18 @@ fun lazy(list: MutableList<Contact>){
     val scope = rememberCoroutineScope()
     val context = LocalContext.current
     val database = Room.databaseBuilder(context, ContactDatabase::class.java,"contacts").build()
-        LazyColumn(state = rememberLazyListState(),
-            content = {
-        items(list){item ->
+    LazyColumn(state = rememberLazyListState(),
+            content = { itemsIndexed(items = list, key = { _, listItem ->
+                listItem.hashCode()
+            }) { index, item ->
 
             val state = rememberDismissState(
                 confirmValueChange = {
                     if(it==DismissValue.DismissedToEnd){
-                        scope.launch {
-                            database.itemDao().deleteContact(item.phoneNumber)
+                        Log.d("deleteContact1",item.firstName)
+                        scope.launch(Dispatchers.IO) {
+                            database.itemDao().deleteContact(item)
+                            Log.d("deleteContact2",item.firstName)
                         }
                     }
                     true
@@ -137,17 +137,29 @@ fun lazy(list: MutableList<Contact>){
                 state = state,
                 background = {
                              val color = when(state.dismissDirection){
-                                 DismissDirection.StartToEnd-> Color.Red
+                                 DismissDirection.StartToEnd-> colorResource(id = R.color.redorange)
                                  DismissDirection.EndToStart-> Color.DarkGray
                                  null -> Color.Transparent
                              }
+                    Box(
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .background(color)
+                            .padding(8.dp)
+                    ){
+                        Icon(imageVector = Icons.Default.Delete, contentDescription = "Delete",
+                            modifier = Modifier.align(Alignment.CenterEnd))
+                    }
                 },
                 dismissContent = {
                     Contacts(fName = item.firstName, lName = item.lastName, pNum = item.phoneNumber,checked = item.emergency)
-                })
-
+                },
+                directions=setOf(DismissDirection.StartToEnd)
+            )
+            Divider()
         }
     })
+
 }
 
 //suspend fun getList(database: ContactDatabase){
@@ -157,4 +169,3 @@ fun lazy(list: MutableList<Contact>){
 //    }
 //    Log.d("Hellllo",list.await().toString())
 //}
-
