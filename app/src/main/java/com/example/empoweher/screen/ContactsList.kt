cda269794f79340f7e2e42a5f7ee2221d76a19c8
Amansande <amansande1934@gmail.com>
2023-11-14 16:21:36 +0530

    Something done

--- app/src/main/java/com/example/empoweher/screen/ContactsList.kt
+++ app/src/main/java/com/example/empoweher/screen/ContactsList.kt
@@ -2,6 +2,7 @@ package com.example.empoweher.screen
 
 import android.annotation.SuppressLint
 import android.util.Log
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
@@ -11,6 +12,7 @@ import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.lazy.rememberLazyListState
@@ -21,6 +23,7 @@ import androidx.compose.material3.DismissDirection
 import androidx.compose.material3.DismissValue
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.SwipeToDismiss
+import com.example.empoweher.R
 import androidx.compose.material3.Text
 import androidx.compose.material3.rememberDismissState
 import androidx.compose.runtime.Composable
@@ -32,6 +35,10 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.room.Room
@@ -39,6 +46,7 @@ import com.example.empoweher.SQLIteDB.Contact
 import com.example.empoweher.SQLIteDB.ContactDatabase
 import com.example.empoweher.SQLIteDB.getList
 import kotlinx.coroutines.CoroutineScope
+import com.example.empoweher.composables.ContactCard
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.async
@@ -61,10 +69,10 @@ fun ContactsList(){
         }
     }
 
-//    Text(text = List.toString())
-
     LazyColumn(
-        modifier=Modifier.fillMaxWidth(),
+        modifier= Modifier
+            .fillMaxSize()
+            .background(colorResource(id = R.color.cream)),
         content = {
             items(List){item->
                 Contacts(fName = item.firstName, lName =item.lastName , pNum =item.phoneNumber , checked =item.emergency )
@@ -84,31 +92,65 @@ suspend fun func(database: ContactDatabase){
 }
 
 @Composable
-fun Contacts(fName: String, lName: String, pNum: String,checked: Boolean){
-    Card(
-        elevation = CardDefaults.cardElevation(defaultElevation = 8.dp) ,
-        modifier = Modifier
-            .padding(8.dp)
-            .fillMaxWidth()
-            .clickable {
+fun Contacts(fName: String, lName: String, pNum: String,checked: Boolean) {
+    if (checked) {
+        Card(
+            elevation = CardDefaults.cardElevation(defaultElevation = 8.dp),
+            colors = CardDefaults.cardColors(
+                containerColor = colorResource(id = R.color.orchid),
+            ),
+            modifier = Modifier
+                .padding(8.dp)
+                .fillMaxWidth()
+                .clickable {
 
+                },
+
+            ) {
+            Column(
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.Center,
+                modifier = Modifier.fillMaxWidth()
+
+            ) {
+                Text(
+                    text = "Name : " + fName + " " + lName,
+                    fontFamily = FontFamily(Font(R.font.font1))
+                )
+                Text(text = "Contact Number : " + pNum, fontFamily = FontFamily(Font(R.font.font1)))
+                Text("Emergency Contact : Yes", fontFamily = FontFamily(Font(R.font.font1)))
             }
-    ){
-        Column(
-            horizontalAlignment = Alignment.CenterHorizontally,
-            verticalArrangement = Arrangement.Center,
-            modifier = Modifier.fillMaxWidth()
-
-        ) {
-            Text(text = "First Name : "+ fName)
-            Text(text = lName)
-            Text(text = pNum)
-            if (checked==true){
-                Text("Emergency Contact")
-            }
-
         }
     }
+        else{
+            Card(
+                elevation = CardDefaults.cardElevation(defaultElevation = 8.dp),
+                colors = CardDefaults.cardColors(
+                    containerColor = colorResource(id = R.color.mauve)
+                ),
+                modifier = Modifier
+                    .padding(8.dp)
+                    .fillMaxWidth()
+                    .clickable {
+
+                    },
+
+                ) {
+                Column(
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                    verticalArrangement = Arrangement.Center,
+                    modifier = Modifier.fillMaxWidth()
+
+                ) {
+                    Text(
+                        text = "Name : " + fName + " " + lName,
+                        fontFamily = FontFamily(Font(R.font.font1))
+                    )
+                    Text(text = "Contact Number : " + pNum, fontFamily = FontFamily(Font(R.font.font1)))
+                    Text("Emergency Contact : No", fontFamily = FontFamily(Font(R.font.font1)))
+                }
+            }
+        }
 }
 
 
@@ -126,7 +168,7 @@ fun lazy(list: MutableList<Contact>){
                 confirmValueChange = {
                     if(it==DismissValue.DismissedToEnd){
                         scope.launch {
-                            database.itemDao().deleteContact(item.phoneNumber)
+                            database.itemDao().deleteContact(item)
                         }
                     }
                     true
