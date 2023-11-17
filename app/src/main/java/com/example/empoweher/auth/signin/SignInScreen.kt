0b8154ae860796de064ff3a859c81865c4653283
Rajveer Tolani <rajveer.tolani@gmail.com>
2023-11-17 17:24:50 +0500

    Login New UI

--- app/src/main/java/com/example/empoweher/auth/signin/SignInScreen.kt
+++ app/src/main/java/com/example/empoweher/auth/signin/SignInScreen.kt
@@ -1,36 +1,62 @@
 package com.example.empoweher.auth.signin
 
 import android.widget.Toast
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.MarqueeAnimationMode
 import androidx.compose.foundation.background
+import androidx.compose.foundation.basicMarquee
+import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.modifier.modifierLocalConsumer
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.vectorResource
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.empoweher.R
+import kotlinx.coroutines.delay
 
+@OptIn(ExperimentalFoundationApi::class)
 @Composable
 fun SignInScreen(
     state: SignInState,
     onSignInClick: () -> Unit,
     navigateToHome: () -> Unit,
 ) {
-    navigateToHome()
+    //navigateToHome()
     val context = LocalContext.current
     LaunchedEffect(key1 = state.signInError) {
         state.signInError?.let { error ->
@@ -42,7 +68,7 @@ fun SignInScreen(
         }
     }
     LaunchedEffect(key1 = state.isSignInSuccessful) {
-        if(state.isSignInSuccessful) {
+        if (state.isSignInSuccessful) {
             navigateToHome()
         }
     }
@@ -52,23 +78,43 @@ fun SignInScreen(
         modifier = Modifier
             .fillMaxSize()
             .background(Color(0xfff7e7d7))
-            .padding(16.dp)
-            ,
+            .padding(16.dp),
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally,
     ) {
         Image(
+            imageVector = ImageVector.vectorResource(id = R.drawable.logo_svg),
+            contentDescription = "Logo",
+            modifier = Modifier
+                .size(90.dp),
+            contentScale = ContentScale.Crop
+        )
+        Image(
             painter = painterResource(id = R.drawable.women_login),
-            contentDescription = "Login",
-            modifier=Modifier
-            .size(390.dp),
+            contentDescription = "Login Pic",
+            modifier = Modifier
+                .size(390.dp),
             contentScale = ContentScale.Fit
         )
         Spacer(modifier = Modifier.height(12.dp))
+        Column(
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally,
+            modifier = Modifier
+                .fillMaxWidth()
+                .background(
+                    color = Color(0xfff7e7d7),
+                ),
+        ) {
+            TypewriterText(
+                texts = listOf(
+                    "Login With Google"
+                ),
+            )
         Image(
             painter = painterResource(id = R.drawable.google_signin),
             contentDescription = "Google",
-            modifier=Modifier
+            modifier = Modifier
                 .size(175.dp)
                 .clickable {
                     onSignInClick()
@@ -76,5 +122,89 @@ fun SignInScreen(
             contentScale = ContentScale.Fit
         )
 
+        }
+        Spacer(modifier = Modifier.height(5.dp))
+
+            Column {
+                Text(text = "About Us",
+                    fontSize= 20.sp,
+                    modifier=Modifier.fillMaxWidth(),textAlign=TextAlign.Center,
+                    fontWeight=FontWeight.Bold,
+                    color = Color(R.color.black),
+                    fontStyle = FontStyle(R.font.font1),
+                )
+                Spacer(modifier = Modifier.height(25.dp))
+                Row(horizontalArrangement = Arrangement.Center,
+                    verticalAlignment = Alignment.CenterVertically,
+                    modifier = Modifier.fillMaxWidth())
+                {
+                   Image(
+                        painter = painterResource(id = R.drawable.insta),
+                        contentDescription = "insta",
+                        modifier = Modifier
+                            .size(60.dp)
+                            .clip(RoundedCornerShape(15.dp)),
+                        contentScale = ContentScale.Fit
+
+
+                    )
+                    Spacer(modifier = Modifier.width(10.dp))
+                    Image(
+                        painter = painterResource(id = R.drawable.facebook),
+                        contentDescription = "facebook",
+                        modifier = Modifier
+                            .size(60.dp)
+                            .clip(RoundedCornerShape(15.dp)),
+                        contentScale = ContentScale.Fit
+                    )
+                    Spacer(modifier = Modifier.width(10.dp))
+                    Image(
+                        painter = painterResource(id = R.drawable.twitter),
+                        contentDescription = "twitter",
+                        modifier = Modifier
+                            .size(60.dp)
+                            .clip(RoundedCornerShape(15.dp)),
+                        contentScale = ContentScale.Fit
+                    )
+            }
+
+        }
+
     }
+
+}
+@Composable
+fun TypewriterText(
+    texts: List<String>,
+) {
+    var textIndex by remember {
+        mutableStateOf(0)
+    }
+    var textToDisplay by remember {
+        mutableStateOf("")
+    }
+
+    LaunchedEffect(
+        key1 = texts,
+    ) {
+        while (textIndex < texts.size) {
+            texts[textIndex].forEachIndexed { charIndex, _ ->
+                textToDisplay = texts[textIndex]
+                    .substring(
+                        startIndex = 0,
+                        endIndex = charIndex + 1,
+                    )
+                delay(160)
+            }
+            textIndex = (textIndex + 1) % texts.size
+            delay(1000)
+        }
+    }
+
+    Text(
+        text = textToDisplay,
+        fontSize = 24.sp,
+        fontWeight = FontWeight.Bold,
+        color = colorResource(id = R.color.orchid)
+    )
 }
