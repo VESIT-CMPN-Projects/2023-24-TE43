9d4790fd577d9965cfea554914b48f91f4da792c
Amansande <amansande1934@gmail.com>
2023-12-18 13:45:19 +0530

    resize 1.3

--- app/src/main/java/com/example/empoweher/screen/Details/Details.kt
+++ app/src/main/java/com/example/empoweher/screen/Details/Details.kt
@@ -179,8 +179,8 @@ private fun GradientButton(
 
 fun converterHeight(old :Int,context:Context):Int{
     val displayMetrics =context.resources.displayMetrics
-    val dpHeight = (displayMetrics.heightPixels / displayMetrics.density).toInt()
-    return old*(dpHeight/914)
+    val dpHeight = (displayMetrics.heightPixels / displayMetrics.density)
+    return (old*(dpHeight/914)).toInt()
 }
 
 
