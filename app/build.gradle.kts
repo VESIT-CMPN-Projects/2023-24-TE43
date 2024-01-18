0bed882fa4e51b077d2cbb35db13a47089f1f554
Amansande <amansande1934@gmail.com>
2024-01-18 20:23:38 +0530

    Video Conference Added Finally

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,7 +14,7 @@ android {
     defaultConfig {
         applicationId = "com.example.empoweher"
         minSdk = 24
-        targetSdk = 34
+        targetSdk = 33
         versionCode = 1
         versionName = "1.0"
 
