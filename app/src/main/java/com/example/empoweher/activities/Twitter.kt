066380ac370c72f0530daf8b5b7a31a4035726d0
Amansande <amansande1934@gmail.com>
2024-03-06 22:03:19 +0530

    python added

---
+++ app/src/main/java/com/example/empoweher/activities/Twitter.kt
@@ -0,0 +1,34 @@
+package com.example.empoweher.activities
+
+import android.os.Bundle
+import android.util.Log
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.chaquo.python.Python
+import com.chaquo.python.android.AndroidPlatform
+import com.example.empoweher.R
+
+class Twitter : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_twitter)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        if (! Python.isStarted()) {
+            Python.start(AndroidPlatform(this));
+        }
+        val py=Python.getInstance()
+        val module=py.getModule("hello")
+        val number=module["number"]?.toInt()
+        val text=module["text"]?.toString()
+        Log.d("Hostel Praful","The value of num is $number")
+        Log.d("Hostel Praful","The value of text is $text")
+
+    }
+}
